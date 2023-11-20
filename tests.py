afd65c56b2a01a6f85520485ae0d55887356995b
Daniil Gordienko <danila.gordi@yandex.ru>
2023-11-21 00:56:59 +0300

    Sprint_4_Доработки

--- tests.py
+++ tests.py
@@ -12,20 +12,20 @@ class TestBooksCollector:
 
         assert len(collector.get_books_genre()) == 2
 
-
-    def test_add_new_book_empty_book(self):
-        # Тест на отсутствие книги
+    @pytest.mark.parametrize("book_name, expect", [
+        ("", False),
+        ("A", True),
+        ("AB", True),
+        ("A" * 39, True),
+        ("A" * 40, True),
+        ("A" * 41, False),
+        ("A" * 42, False)
+    ])
+    def test_add_new_book_different_length_book_name(self, book_name, expect):
+        # тест на размер названия книги
         collector = BooksCollector()
-        book_name = ""
         collector.add_new_book(book_name)
-        assert book_name not in collector.get_books_genre()
-
-    def test_add_add_new_book_long_book_name(self):
-        # Тест на длинное название
-        collector = BooksCollector()
-        book_name = "КакоеТоДлинноеНазваниеДляКнигиБольше41Символов"
-        collector.add_new_book(book_name)
-        assert book_name not in collector.get_books_genre()
+        assert (book_name in collector.get_books_genre()) == expect
 
     def test_set_book_genre_fantasy_book(self):
         # Тест на установку жанра для книги
@@ -35,27 +35,32 @@ class TestBooksCollector:
         result = collector.get_book_genre("Мастер и Маргарита")
         assert result == "Фантастика"
 
-    @pytest.mark.parametrize("book_name, genre", [
-        ("Мастер и Маргарита", "Фантастика"),
-        ("Тихий Дон", "Драма"),
-        ("Оно", "Ужасы"),
-    ])
-
-    def test_get_book_genre_by_name(self, book_name, genre):
+    def test_get_book_genre_by_name(self):
         # Тест на достоверонсть получения жанра книги по её имени
         collector = BooksCollector()
+        book_name = "Мастер и Маргарита"
+        genre = "Фантастика"
+
         collector.add_new_book(book_name)
         collector.set_book_genre(book_name, genre)
         result = collector.get_book_genre(book_name)
+
         assert result == genre
 
     def test_books_for_children_include_non_age_rated_books(self):
         # Проверка, что книги без возрастного рейтинга попадают в список книг для детей
         collector = BooksCollector()
-        collector.add_new_book("Сказки")
-        collector.set_book_genre("Сказки", "Мультфильмы")
-        result = collector.get_books_for_children()
-        assert "Сказки" in result
+        children_books = {
+            "Сказки Пушкина": "Мультфильмы",
+            "Юмористические рассказы": "Комедии",
+            "Метро 2033": "Фантастика"
+        }
+
+        for book, genre in children_books.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+
+        assert collector.get_books_for_children() == ['Сказки Пушкина', 'Юмористические рассказы', 'Метро 2033']
 
     def test_books_for_children_exclude_age_rated_books(self):
         # Проверка, что книги с возрастным рейтингом не попадают в список книг для детей
@@ -74,10 +79,19 @@ class TestBooksCollector:
     def test_get_books_with_specific_genre_by_book(self):
         # Тест на получение списка книг с заданным жанром
         collector = BooksCollector()
-        collector.add_new_book("Cказки")
-        collector.set_book_genre("Сказки", "Мультфильмы")
-        result = collector.get_books_with_specific_genre("Мультфильмы")
-        assert result == ["Сказки"]
+        books_data = {
+            "Мастер и Маргарита": "Фантастика",
+            "Тихий Дон": "Драма",
+            "Оно": "Ужасы",
+            "Война и мир": "История"
+        }
+
+        for book, genre in books_data.items():
+            collector.add_new_book(book)
+            collector.set_book_genre(book, genre)
+
+        result = collector.get_books_with_specific_genre("Ужасы")
+        assert result == ["Оно"]
 
     def test_get_books_genre_by_book(self):
         # Тест на получение текущего словаря по книге
